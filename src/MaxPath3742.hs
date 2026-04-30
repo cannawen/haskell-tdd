a0dcb0759b95c28438fb03cc683caf219bf7e2c0
Canna Wen <cannawen@gmail.com>
2026-04-30 17:30:22 -0400

    Have Claude refactor to use array

--- src/MaxPath3742.hs
+++ src/MaxPath3742.hs
@@ -1,53 +1,50 @@
-module MaxPath3742 
+module MaxPath3742
   (maxPath) where
-import Data.Maybe (catMaybes, maybeToList)
+
+import Data.Array
 import Data.Function
 import Data.List
-import Control.Monad
 
 type Row = Int
 type Column = Int
 type Cost = Int
-type Point = (Row, Column, Cost)
+type Coord = (Row, Column)
+type Grid = Array Coord Cost
 
 pathScores :: [Cost] -> (Int, Int)
-pathScores path = foldl' updateFn (0,0) path
+pathScores = foldl' updateFn (0, 0)
   where
     updateFn (score, cost) 2 = (score + 2, cost + 1)
     updateFn (score, cost) 1 = (score + 1, cost + 1)
     updateFn (score, cost) _ = (score, cost)
 
 maxPath :: [[Int]] -> Int -> Int
-maxPath grid maxCost = if not (null p) then maximum p else -1
-  where 
-    rawPathCosts = map (\path -> (map (\(_,_,cost) -> cost) path)) (allPaths (parse grid)) 
-    p = 
-      map pathScores rawPathCosts 
-      & filter (\(_, cost) -> cost <= maxCost) 
+maxPath rawGrid maxCost = if not (null p) then maximum p else -1
+  where
+    grid = parse rawGrid
+    p = allPaths grid
+      & map (map (grid !))
+      & map pathScores
+      & filter (\(_, cost) -> cost <= maxCost)
       & map fst
 
-parse :: [[Int]] -> [[Point]]
-parse grid = [[(x, y, cost) | (y, cost) <- zip [0..] row] | (x, row) <- zip [0..] grid]
-
-allPaths :: [[Point]] -> [[Point]]
-allPaths grid = go (grid !! lastRow !! lastCol)
+parse :: [[Int]] -> Grid
+parse rawGrid = listArray ((0, 0), (lastRow, lastCol)) (concat rawGrid)
   where
-    lastRow = length grid - 1
-    lastCol = length (head grid) - 1
-    go point@(x,y,_)
-      | x == 0 && y == 0 = [[point]]
-      | otherwise = do
-        next <- nextPoints grid point
-        path <- go next
-        return (point : path)
+    lastRow = length rawGrid - 1
+    lastCol = length (head rawGrid) - 1
 
-nextPoints :: [[Point]] -> Point -> [Point]
-nextPoints grid (px, py, pcost) = do
-  (dx, dy) <- [(-1, 0), (0, 1)]
-  maybeToList (getPointInGrid grid (px + dx) (py + dy))
-  
-getPointInGrid :: [[Point]] -> Row -> Column -> Maybe Point
-getPointInGrid grid x y = do
-  guard (x >= 0 && y >= 0)
-  return $ grid !! x !! y
-  
+allPaths :: Grid -> [[Coord]]
+allPaths grid = go (lastRow, lastCol)
+  where
+    (_, (lastRow, lastCol)) = bounds grid
+    go coord
+      | coord == (0, 0) = [[coord]]
+      | otherwise = do
+          next <- nextPoints grid coord
+          path <- go next
+          return (coord : path)
+
+nextPoints :: Grid -> Coord -> [Coord]
+nextPoints grid (px, py) =
+  filter (inRange (bounds grid)) [(px - 1, py), (px, py - 1)]
