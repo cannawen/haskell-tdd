af0900d7feeb823e3c3db4afea150cd64b0d8c20
Canna Wen <cannawen@gmail.com>
2026-05-01 14:18:14 -0400

    This was easy? But they're saying there's a DP solution? Gross.

---
+++ src/Rotate396.hs
@@ -0,0 +1,16 @@
+module Rotate396 () where
+import Data.Function
+import Data.List
+
+rotate :: [Int] -> Int
+rotate arr = 
+    [zip [0..] (rotateBy n arr) | n <- [0..length arr]]
+    & map calculateProductSum 
+    & maximum
+    
+    where
+        rotateBy :: Int -> [a] -> [a]
+        rotateBy n arr = drop n arr ++ take n arr
+        
+        calculateProductSum :: [(Int,Int)] -> Int
+        calculateProductSum arr = map (\(i,n) -> i * n ) arr & foldl1' (+)
