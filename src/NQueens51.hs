bb86d7241c21d85855a4dd9187f1c54c9f4f38a4
Canna Wen <cannawen@gmail.com>
2026-05-03 18:15:16 -0400

    Getting the board state but many duplicates

--- src/NQueens51.hs
+++ src/NQueens51.hs
@@ -7,23 +7,41 @@ import Lib
 
 type Row = Int
 type Column = Int
-data Cell = Queen | Open | Closed
-type Board =  [[(Row, Column, Cell)]]
+type QueensRemaining = Int
+data Cell = Queen | Valid | Invalid deriving (Eq)
+
+instance Show Cell where
+  show Queen   = "Q"
+  show Valid   = " "
+  show Invalid = "."
+
+type Board =  [[Cell]]
 
 -- calculateNQueens :: Int -> Int
-calculateNQueens n = calculateNQueens' (createGrid n)
+calculateNQueens n = calculateNQueens' (n-1) (createGrid (n-1))
 
-calculateNQueens' board = do
-    row <- board
-    (x, y, cell) <- row
-    if cell == Open
-        then calculateNQueens (addQueen x y board)
-        else board
+calculateNQueens' :: Int -> Board -> [Board]
+calculateNQueens' n board = 
+    if boardFull board && countQueens board == n + 1
+        then [board]
+        else concatMap (\board -> calculateNQueens' n board) possibleFutures
+    where 
+        openCells = [(x,y) | x <- [0..n], y <- [0..n], board !! x !! y == Valid]
+        possibleFutures = map (\(x,y) -> addQueen n x y board) openCells
+        boardFull board = not $ any (== Valid) (concat board)
 
-addQueen :: Int -> Int -> Board -> Board
-addQueen x y board = board --TODO
+countQueens board = filter (\c -> c == Queen) (concat board) & length
+
+addQueen :: Int -> Row -> Column -> Board -> Board
+addQueen n x y board = [[newCell r c | c <- [0..n]] | r <- [0..n]]
+  where
+    newCell r c
+      | r == x && c == y = Queen
+      | r == x || c == y = Invalid
+      | abs (r - x) == abs (c - y) = Invalid
+      | otherwise = board !! r !! c
 
 createGrid :: Int -> Board
-createGrid n = [[(x,y,Open) | x <- [0..n]] | y <- [0..n]]
+createGrid n = [[Valid | x <- [0..n]] | y <- [0..n]]
 
 
