96bf760ba348e7ef0b0a5785ee7ce73045b35390
Canna Wen <cannawen@gmail.com>
2026-05-02 21:03:46 -0400

    Add second way to calculate stuff - more explicit

--- src/RotatedDigits788.hs
+++ src/RotatedDigits788.hs
@@ -18,7 +18,37 @@ isValid int = rotatedInt /= Nothing && rotatedInt /= Just integerString
     where 
         integerString = show int
         rotatedInt = map rotateChar integerString & sequence
-    
 
 rotateUntil :: Int -> Int
 rotateUntil n = map isValid [0..n] & filter id & length
+
+isSame :: Char -> Bool
+isSame '0' = True
+isSame '1' = True
+isSame '2' = False
+isSame '3' = False
+isSame '4' = False
+isSame '5' = False
+isSame '6' = False
+isSame '7' = False
+isSame '8' = True
+isSame '9' = False
+
+isRotatable :: Char -> Bool
+isRotatable '0' = True
+isRotatable '1' = True
+isRotatable '2' = True
+isRotatable '3' = False
+isRotatable '4' = False
+isRotatable '5' = True
+isRotatable '6' = True
+isRotatable '7' = False
+isRotatable '8' = True
+isRotatable '9' = True
+
+isValid' :: Int -> Bool
+isValid' int = rotatable && not same
+    where 
+        intString = show int
+        rotatable = map isRotatable intString & and
+        same = map isSame intString & and
