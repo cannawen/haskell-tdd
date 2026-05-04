7bf352c262765f8ca8a05eb554cd2d84621455c1
Canna Wen <cannawen@gmail.com>
2026-05-04 14:30:03 -0400

    Unrolled 4-Queens

--- src/NQueens51.hs
+++ src/NQueens51.hs
@@ -6,6 +6,7 @@ import Data.Array
 import Data.Function
 import Data.List
 import Lib
+import Control.Applicative (Alternative(empty))
 
 type Row = Int
 type Column = Int
@@ -61,10 +62,27 @@ isValid :: BoardSize -> [QueenCoordinate] -> Bool
 isValid n board = and $ map (\coord -> map (isValidInDir n board coord) deltas & and) board
 
 deltas :: [(Row, Column)]
-deltas = [(dx, dy) | dx <- [-1, 1], dy <- [-1, 1]]
+deltas = [(dx, dy) | dx <- [-1, 0, 1], dy <- [-1, 0, 1], (dx, dy) /= (0, 0)]
 
 isValidInDir :: Int -> [(Row, Column)] -> (Row, Column) -> (Row, Column) -> Bool
 isValidInDir n board start dir = 
     (r >= n || c >= n || r < 0 || c < 0 ) ||
     not (index `elem` board)  && isValidInDir n board index dir
     where index@(r, c) = (fst start + fst dir, snd start + snd dir)
+
+-------------------------------------------------------------------------------
+
+-- nQueens :: BoardSize -> Int
+nQueens n = board3
+    where
+        queenPosRow0 = [(0,0),(0,1),(0,2),(0,3)]
+        board0 = [[(0,0)],[(0,1)],[(0,2)],[(0,3)]]
+
+        queenPosRow1 = [(1,0),(1,1),(1,2),(1,3)]
+        board1 = concatMap (\board -> map (\q -> if isValid 4 (q:board) then q:board else []) queenPosRow1) board0 & filter (not . null)
+
+        queenPosRow2 = [(2,0),(2,1),(2,2),(2,3)]
+        board2 = concatMap (\board -> map (\q -> if isValid 4 (q:board) then q:board else []) queenPosRow2) board1 & filter (not . null)
+
+        queenPosRow3 = [(3,0),(3,1),(3,2),(3,3)]
+        board3 = concatMap (\board -> map (\q -> if isValid 4 (q:board) then q:board else []) queenPosRow3) board2 & filter (not . null)
