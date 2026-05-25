d566ba40f39411b35cf5e4153de2896fc31c790e
Canna Wen <cannawen@gmail.com>
2026-05-25 19:28:07 -0400

    Add Jump Game VII implementation with min jump logic and tests

--- src/JumpGameVII1871.hs
+++ src/JumpGameVII1871.hs
@@ -1,4 +1,16 @@
 module JumpGameVII1871
   (canReachEnd) where
 
-canReachEnd string minJump maxJump = True
+-- canReachEnd string minJump maxJump = validJump string minJump
+
+canReachEnd :: [Char] -> Int -> Int -> Bool
+canReachEnd string minJump maxJump = 
+  if string == ""
+    then True
+    else if validJump string minJump
+    then canReachEnd (drop (succ minJump) string) minJump maxJump
+    else False
+
+validJump :: [Char] -> Int -> Bool
+validJump string jump = string !! jump == '0'
+
--- test/JumpGameVIISpec.hs
+++ test/JumpGameVIISpec.hs
@@ -5,5 +5,10 @@ import JumpGameVII1871 (canReachEnd)
 
 spec :: Spec
 spec = do 
-  it "works" $ do
-    canReachEnd "10" 1 1 `shouldBe` True
+  describe "min jumping" $ do
+    it "should be able to jump to 0 index" $ do
+      canReachEnd "10" 1 10 `shouldBe` True
+      canReachEnd "1010" 1 10 `shouldBe` True
+    it "should not be able to jump to 1 index" $ do
+      canReachEnd "11" 1 10 `shouldBe` False
+      canReachEnd "1011" 1 10 `shouldBe` False
