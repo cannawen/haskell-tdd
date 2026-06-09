4c5359a2e01ca2c39a27b8f421dcb61982914b97
Canna Wen <cannawen@gmail.com>
2026-06-09 10:22:25 -0400

    All the various ways to get all subarrays Fixed issue with taking wrong number of elements

--- src/MaximumTotalSubarrayValueI3689.hs
+++ src/MaximumTotalSubarrayValueI3689.hs
@@ -16,7 +16,25 @@ allSubarr :: [Int] -> [[Int]]
 allSubarr nums = do
     l <- [0..pred len]
     r <- [succ l..len]
-    return (drop l nums & take r)
+    return (drop l nums & take (r-l))
+    where len = length nums
+
+allSubarr'' :: [Int] -> [[Int]]
+allSubarr'' nums = [drop l nums & take (r-l) | l <- [0..pred len], r <- [succ l..len]]
+    where len = length nums
+
+allSubarr' :: [Int] -> [[Int]]
+allSubarr' nums = 
+    map 
+    (\l -> 
+        map 
+        (\r -> 
+            drop l nums & take (r-l)) 
+        [succ l..len]
+    ) 
+    [0..pred len]
+    & concat
+
     where len = length nums
 
 value :: [Int] -> Int
